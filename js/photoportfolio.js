4cde324db80dd4ca7357ef8146361e0a0d31400c
meirna <mpiberni@gmail.com>
2016-06-19 09:32:18 +0200

    Update photoportfolio.js

--- js/photoportfolio.js
+++ js/photoportfolio.js
@@ -1,3 +1,5 @@
+<script>
+
 var slideIndex = 0;
 carousel();
 
@@ -12,3 +14,5 @@ function carousel() {
     x[slideIndex-1].style.display = "block"; 
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+</script>
