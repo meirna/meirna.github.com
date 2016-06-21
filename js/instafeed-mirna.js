a4a00e00d5fec6f103664a09f24e57e2ede9685a
meirna <mpiberni@gmail.com>
2016-06-21 14:30:19 +0200

    Create instafeed-mirna.js

---
+++ js/instafeed-mirna.js
@@ -0,0 +1,7 @@
+    var feed = new Instafeed({
+        get: 'user',
+        userId: '223043938',
+        clientId: 'a9b9c8b243f64c0987612bc35d523cf1'
+        template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+    });
+    feed.run();
